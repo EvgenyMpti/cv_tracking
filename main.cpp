74629bda6a74c25ccf6e399bdb142b137fca56f7
EvgenyMpti <limarenko.ev@phystech.edu>
2024-12-18 18:13:24 +0300

    cleaning main.cpp

--- main.cpp
+++ main.cpp
@@ -5,48 +5,6 @@
 
 #include "model/clinic.h"
 
-/*
-cv::Mat alignFace(const cv::Mat& src, obj::Body& body)
-{
-    auto face_kp = body.GetFaceKP();
-    const auto face_rect = *body.GetFace();
-    //cv::Mat face_mat = src(face_rect);
-    //for (auto& kp : face_kp)
-    //    kp -= face_rect.tl();
-
-    // Целевые координаты (приблизительно)
-    std::vector<cv::Point2f> dst_pts = {
-        face_kp[0],  // Нос (центр лица по вертикали, 1/3 высоты)
-        face_kp[1],  // Левый глаз
-        face_kp[2],   // Правый глаз
-        face_kp[3],  // Левое ухо (четверть ширины, середина высоты)
-        cv::Point2f(face_rect.width * 3 / 4, face_rect.height / 2)  // Правое ухо
-    };
-    
-    // Вычисление средних координат и расстояний
-    float avg_eye_y = (face_kp[1].y + face_kp[2].y) / 2;
-    float avg_ear_y = (face_kp[3].y + face_kp[4].y) / 2;
-
-    // Корректировка целевых координат на основе средних
-    //dst_pts[1].y = dst_pts[2].y = avg_eye_y;
-    //dst_pts[3].y = dst_pts[4].y = avg_ear_y;
-
-    // Вычисление матрицы гомографии
-    //cv::Mat H = findHomography(face_kp, dst_pts);
-
-    // Применение преобразования
-    //cv::Mat warped_img;
-    //warpPerspective(face_mat, warped_img, H, face_rect.size());
-    //cv::circle(warped_img, dst_pts[4], 2, cv::Scalar(0, 255, 0), -1);
-    //////////////////////////////
-
-    //std::vector<cv::Point2d> image_points = face_kp;
-
-    return {};// warped_img;
-}
-
-*/
-
 void processCamera(CamSec::Clinic& clinic, const CamSec::Camera* camera) {
 
     cv::VideoCapture cap(camera->GetSource());
